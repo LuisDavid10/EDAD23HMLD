ab1d2749f37d8c1896317a310eded54a0b0c0ff7
LuisDavid10 <L22050369@saltillo.tecnm.mx>
2023-09-13 10:36:30 -0600

    Se agrega aplicacion 13/09/23

---
+++ src/aplicacion/AplicacionPilaAcotada.java
@@ -0,0 +1,30 @@
+package aplicacion;
+
+import estructuras.PilaAcotada;
+//import java.util.Scanner;
+import java.lang.*;
+
+/**
+ * 
+ */
+public class AplicacionPilaAcotada {
+/**
+ * 
+ */
+public static void main(String[] args){
+    //declarar variables de referencia
+    PilaAcotada pila;
+    Scanner teclado;
+
+    //declara variables asociadas a los objetos
+    int cantidad;
+    char elemntoAInsertar;
+    boolean isEmpty,isFull;
+
+    //variables asociadas a la solucion
+
+    //crear objetos
+    teclado = new Scanner(System.in);
+
+}
+}
