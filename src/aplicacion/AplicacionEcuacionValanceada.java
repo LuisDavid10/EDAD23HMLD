660a27e9f25e3134a751524ff62259b009b82142
LuisDavid10 <L22050369@saltillo.tecnm.mx>
2023-09-19 17:34:31 -0600

    Copia de seguridad 19/09/23

---
+++ src/aplicacion/AplicacionEcuacionValanceada.java
@@ -0,0 +1,9 @@
+/**
+ * 
+*escribir una aplicacion que determine si una ecuacion esta valanceada en parentecis
+*no puedes cerrar si no esta abierto  sacar sin tener excepcion pop
+*usa una pila
+*expresion: operandos combinados mediante operadores que te dan un resultado
+*los operandos pueden ser variables, constantes literales o simbolicas e incluso operadores
+*
+ */
--- src/aplicacion/AplicacionPilaAcotada.java
+++ src/aplicacion/AplicacionPilaAcotada.java
@@ -1,18 +1,20 @@
 package aplicacion;
 
 import estructuras.PilaAcotada;
-//import java.util.Scanner;
-import java.lang.*;
+import java.util.Scanner;
 
 /**
- * 
- */
+*
+*@autor
+*@version 
+*/
 public class AplicacionPilaAcotada {
 /**
  * 
  */
-public static void main(String[] args){
+public static void main(String[] args)  {
     //declarar variables de referencia
+    //estos son los objetos 
     PilaAcotada pila;
     Scanner teclado;
 
@@ -25,6 +27,44 @@ public static void main(String[] args){
 
     //crear objetos
     teclado = new Scanner(System.in);
+    pila = new PilaAcotada(2);
+    
+    //entrada
+    //intentar solicitar ejecucion de metodo que puede generar una exception 
+    //a treves de un "bloque protegido" 
+    //usando try
+    try{
+        pila.push('a');
+    }catch(Exception a){
+        // en caso que se genere una exception
+        //la capturo para dar el flujo de exceptiones de error
+        System.out.println(a.getMessage());
+    }
+    try{
+        pila.push('b');
+    }catch(Exception b){
+        // en caso que se genere una exception
+        //la capturo para dar el flujo de exceptiones de error
+        System.out.println(b.getMessage());
+    }
+    try{
+        pila.push('c');
+    }catch(Exception c ){
+        // en caso que se genere una exception
+        //la capturo para dar el flujo de exceptiones de error
+        System.out.println(c.getMessage());
+    }
 
+    //proceso
+
+    //salida
+    //no siempre puedes eliminar
+    try{
+    System.out.println(pila.pop());
+    System.out.println(pila.pop());
+    System.out.println(pila.pop());
+    }catch(Exception e ){
+        System.err.println(e.getMessage());
+    }
 }
 }
